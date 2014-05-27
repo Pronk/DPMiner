d3d5569e7548ab54abddb5e6afab5a50658a6c99
Pronko <n.pronko1@gmail.com>
2014-05-27 23:39:39 +0400

    Vertices for visual Petri nets

--- DPMiner/DPMiner/Petri.cs
+++ DPMiner/DPMiner/Petri.cs
@@ -20,28 +20,28 @@ namespace DPMiner
     }
     class PetriNet : IPetriNet, IEquatable<PetriNet>
     {
-        
+
         int places;
         int[] markup;
         int tc;
         int[,] transitions;
-        
+
         // naive compare
-        public  bool Equals(PetriNet that)
+        public bool Equals(PetriNet that)
         {
-            if(this.tc != that.tc )
+            if (this.tc != that.tc)
                 return false;
-            if(this.places != that.places)
+            if (this.places != that.places)
                 return false;
-            for(int i = 0; i<tc;i++)
-                for(int j = 0; j<places; j++)
-                    if(this.transitions[i,j] != that.transitions[i,j] )
+            for (int i = 0; i < tc; i++)
+                for (int j = 0; j < places; j++)
+                    if (this.transitions[i, j] != that.transitions[i, j])
                         return false;
             return true;
         }
         public PetriNet(int places, int tc)
         {
-            if(places<0 || tc < 0)
+            if (places < 0 || tc < 0)
                 throw new ArgumentException("numbers must be positive!");
             this.places = places;
             this.tc = tc;
@@ -49,81 +49,88 @@ namespace DPMiner
             transitions = new int[tc, places];
         }
 
-       public Maybe<PetriNet> setMarkup( int[] markup)
-       {
-           if(markup == null)
-               throw new ArgumentNullException();
-           if(markup.Length != places)
-               return Maybe<PetriNet>.None();
-           this.markup = markup;
-           return this;
-       }
-       public Maybe<PetriNet> setTransitions(int[,] transitions)
-       {
-           if(transitions == null)
-               throw new ArgumentNullException();
-           Tuple<int,int> size =Program.Util.MeasureSize(transitions);
-           if( size.Item2 != places || size.Item1 != tc )
-               return Maybe<PetriNet>.None();
-           this.transitions = transitions;
-           return this;
-       }
-
-      private void Fire( int transID )
-      {
-          for(int i = 0; i < places; i ++ )
-              markup[i]+= transitions[transID, i];
-          
-      }
-      private bool IsFirable(int transID)
-      {
-          for(int i = 0; i < places; i ++ )
-              if(transitions[transID, i] < 0 && markup[i] < -transitions[transID, i])
-                  return false;
-          return true;
-      }
-      public IPetriControl GetControls()
-      {
-          return new PetriControl(this);
-      }                                   
-      public class PetriControl:IPetriControl
-      {
-          PetriNet model;
-          public PetriControl(PetriNet net)
-          {
-              model = net;
-          }
-          public int[] Firable()
-          {
-              List<int> firable = new List<int>();
-             for(int i=0 ; i<model.places; i++ )
-                 if(model.IsFirable(i))
-                     firable.Add(i);
-             return firable.ToArray();
-          }
-          public bool TryFire(int id)
-          {
-              if(model.IsFirable(id))
-              {
-                  model.Fire(id);
-                  return true;
-              }
-              return false;
-
-         }
-        public bool Act()
+        public Maybe<PetriNet> setMarkup(int[] markup)
         {
-            Random gen = new Random();
-            int[] actions =  Firable();
-            if(actions.Length == 0)
-                return false;
-            model.Fire(gen.Next(0, actions.Length));
-            return true;
-                
+            if (markup == null)
+                throw new ArgumentNullException();
+            if (markup.Length != places)
+                return Maybe<PetriNet>.None();
+            this.markup = markup;
+            return this;
+        }
+        public Maybe<PetriNet> setTransitions(int[,] transitions)
+        {
+            if (transitions == null)
+                throw new ArgumentNullException();
+            Tuple<int, int> size = Program.Util.MeasureSize(transitions);
+            if (size.Item2 != places || size.Item1 != tc)
+                return Maybe<PetriNet>.None();
+            this.transitions = transitions;
+            return this;
         }
 
-      }
+        private void Fire(int transID)
+        {
+            for (int i = 0; i < places; i++)
+                markup[i] += transitions[transID, i];
+
+        }
+        private bool IsFirable(int transID)
+        {
+            for (int i = 0; i < places; i++)
+                if (transitions[transID, i] < 0 && markup[i] < -transitions[transID, i])
+                    return false;
+            return true;
+        }
+        public IPetriControl GetControls()
+        {
+            return new PetriControl(this);
+        }
+        public class PetriControl : IPetriControl
+        {
+            PetriNet model;
+            public PetriControl(PetriNet net)
+            {
+                model = net;
+            }
+            public int[] Firable()
+            {
+                List<int> firable = new List<int>();
+                for (int i = 0; i < model.places; i++)
+                    if (model.IsFirable(i))
+                        firable.Add(i);
+                return firable.ToArray();
+            }
+            public bool TryFire(int id)
+            {
+                if (model.IsFirable(id))
+                {
+                    model.Fire(id);
+                    return true;
+                }
+                return false;
+
+            }
+            public bool Act()
+            {
+                Random gen = new Random();
+                int[] actions = Firable();
+                if (actions.Length == 0)
+                    return false;
+                model.Fire(gen.Next(0, actions.Length));
+                return true;
+
+            }
+
+        }
+        public partial class VisualPetri
+        {
+            PetriNet model;
+            public VisualPetri(PetriNet model)
+            {
+                this.model = model;
+            }
+        }
     }
-      
     }
 
--- DPMiner/DPMiner/VisualPetri.cs
+++ DPMiner/DPMiner/VisualPetri.cs
@@ -10,16 +10,23 @@ using System.Drawing.Drawing2D;
 
 namespace DPMiner
 {
-    class VisualPetri
+    public partial class VisualPetri
     {
-       
+        List<Drawable> elements;
+        Drawable choosen;
+        protected void GetElements()
+        {
+            
+        }
     }
     public abstract class Drawable
     {
+        
         public abstract void Draw(Graphics canvus);
+        public abstract bool IsCaught(Point point);
         protected int x;
         protected int y;
-         public Point Location
+        public virtual Point Location
         {
             get { return new Point(x, y); }
             set { x = value.X; y = value.Y;}
@@ -41,6 +48,10 @@ namespace DPMiner
             this.x = x;
             this.y = y;
             this.markup = markup;
+        }
+        public override bool IsCaught(Point p)
+        {
+            return (p.X - x <= 64) && (p.Y - y <= 64);
         }
         public override void Draw(Graphics canvus)
         {
@@ -96,11 +107,113 @@ namespace DPMiner
             this.y = y;
             this.name = name;
         }
+        public override bool IsCaught(Point p)
+        {
+            return (p.X - x <= 25 && p.X -x > -5) && (p.Y - y <= 65 && p.Y - y > -5);
+        }
         public override void Draw(Graphics canvus)
         {
-            canvus.DrawRectangle(new Pen(Color.Black), x, y, 40, 80);
-            Font myFont = new Font("Helvetica", 20);
-            canvus.DrawString(name, myFont, new SolidBrush(Color.Black), new PointF(x - (name.Length - 1)*5, y - 22));
+            canvus.DrawRectangle(new Pen(Color.Black, 6), x, y, 20, 60);
+            Font myFont = new Font("Helvetica", 15);
+            canvus.DrawString(name, myFont, new SolidBrush(Color.Black), new PointF(x - (name.Length - 1) * 5, y - 26));
+            
         }
     }
-}
+    public class Arrow:Drawable
+    {
+        int weight;
+        Node node;
+        Transition trans;
+        public Arrow(Node node, Transition trans, int weight)
+        {
+            this.weight = weight;
+            this.node = node;
+            this.trans = trans;
+        }
+        public override bool IsCaught(Point point)
+        {
+            return false;
+        }
+        public override void Draw(Graphics canvus)
+        {
+            PointF begin;
+            PointF end;
+            PointF scnd;
+            PointF third;
+            bool toNode = node.Location.X > trans.Location.X;
+            if (toNode)
+            {
+                begin = new PointF(node.Location.X - 2, node.Location.Y + 32);
+                end = new PointF(trans.Location.X + 22, trans.Location.Y + 30);
+                scnd = new PointF((float)(node.Location.X - Coef() * (node.Location.X - trans.Location.X)), node.Location.Y + 32);
+                third = new PointF((float)(trans.Location.X + Coef() * (node.Location.X - trans.Location.X)), trans.Location.Y + 30);
+            }
+            else
+            {
+                begin = new Point(node.Location.X + 66, node.Location.Y + 32);
+                end = new Point(trans.Location.X - 2, trans.Location.Y + 30);
+                scnd = new PointF((float)(node.Location.X - Coef() * (node.Location.X - trans.Location.X)), node.Location.Y + 32);
+                third = new PointF((float)(trans.Location.X + Coef() * (node.Location.X - trans.Location.X)), trans.Location.Y + 30);
+            }
+            canvus.DrawBezier(new Pen(Color.Black, 4), begin, scnd, third, end);
+            if (toNode && weight > 0)
+            {
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X - 15, begin.Y + 5), new PointF(begin.X - 2, begin.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X - 15, begin.Y - 5), new PointF(begin.X - 2, begin.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X - 12, begin.Y - 3), new PointF(begin.X - 12, begin.Y + 3));
+                if (weight > 1)
+                    canvus.DrawString(weight.ToString(), new Font("Impact", 15, FontStyle.Italic), new SolidBrush(Color.Blue), new PointF(end.X + (float)0.1 * (1 / Coef()) * Location.X, end.Y + (float)0.1 * (1 / Coef()) * Location.Y));
+                return;
+            }
+            if (weight > 0)
+            {
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X + 15, begin.Y + 5), new PointF(begin.X + 2, begin.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X + 15, begin.Y - 5), new PointF(begin.X + 2, begin.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(begin.X + 12, begin.Y - 3), new PointF(begin.X + 12, begin.Y + 3));
+                if (weight > 1)
+                    canvus.DrawString(weight.ToString(), new Font("Impact", 15, FontStyle.Italic), new SolidBrush(Color.Blue), new PointF(end.X + (float)0.1 * (1 / Coef()) * Location.X, end.Y + (float)0.1 * (1 / Coef()) * Location.Y));
+                return;
+            }
+            if (toNode)
+            {
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X + 15, end.Y + 5), new PointF(end.X + 2, end.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X + 15, end.Y - 5), new PointF(end.X + 2, end.Y));
+                canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X + 12, end.Y - 3), new PointF(end.X + 12, end.Y + 3));
+                if (weight < -1)
+                    canvus.DrawString((-weight).ToString(), new Font("Impact", 15, FontStyle.Italic), new SolidBrush(Color.Blue), new PointF(begin.X - (float)0.1 * (1 / Coef()) * Location.X, begin.Y - (float)0.1 * (1 / Coef()) * Location.Y));
+                return;
+            }
+            canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X - 15, end.Y + 5), new PointF(end.X - 2, end.Y));
+            canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X - 15, end.Y - 5), new PointF(end.X - 2, end.Y));
+            canvus.DrawLine(new Pen(Color.Black, 4), new PointF(end.X - 12, end.Y - 3), new PointF(end.X - 12, end.Y + 3));
+            if (weight < -1)
+                canvus.DrawString((-weight).ToString(), new Font("Impact", 15, FontStyle.Italic), new SolidBrush(Color.Blue), new PointF(begin.X - (float)0.1 * (1 / Coef()) * Location.X, begin.Y - (float)0.1 * (1 / Coef()) * Location.Y));
+            return;
+        }
+        protected float Distance(Point a, Point b)
+        {
+            return (float)(Math.Sqrt(Math.Pow((a.X - b.X), 2) + Math.Pow((a.Y - b.Y), 2)));
+        }
+
+        protected float Coef()
+        {
+            float x = Math.Abs(node.Location.X - trans.Location.X);
+            float y = Math.Abs(node.Location.Y - trans.Location.Y);
+            float d = Distance(node.Location, trans.Location);
+            if (d == 0.0)
+                return 0;
+            if (x > y)
+                return y / d;
+            else
+                return x / d;
+
+
+        }
+        public override Point Location
+        {
+            get { return new Point(node.Location.X - trans.Location.X, node.Location.Y - trans.Location.Y); }
+            set { }
+        }
+        }
+    }
+
